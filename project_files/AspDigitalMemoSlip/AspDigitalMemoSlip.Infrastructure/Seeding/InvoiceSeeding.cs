e5278a6669ba73e6f293594efb27de6904daad01
vincent-dujardin <45260613+vincent-dujardin@users.noreply.github.com>
2024-01-17 18:55:00 +0100

    link veranderen

--- project_files/AspDigitalMemoSlip/AspDigitalMemoSlip.Infrastructure/Seeding/InvoiceSeeding.cs
+++ project_files/AspDigitalMemoSlip/AspDigitalMemoSlip.Infrastructure/Seeding/InvoiceSeeding.cs
@@ -14,7 +14,7 @@ namespace AspDigitalMemoSlip.Infrastructure.Seeding
                 Total = 100.00,
                 Commision = 10.00,
                 CommisionStatus = Status.Paid,
-                ConsigneeId = "02174cf0–9412–4cfe-afbf-59f706d72cf7",
+                ConsigneeId = "02174cf0–9412–4cfe-afbf-59f706d72cf6",
                 ConsignerId = "341743f0-asd2–42de-afbf-59kmkkmk72cf6",
 
 
